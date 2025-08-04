9b88c613a8b53f28afc015eb2157969baeb5f1d4
Sebastian Certuche <sebascertuche@gmail.com>
2025-08-04 09:17:43 -0600

    Add binary exponentiation implementation

---
+++ src/math/binexp.hpp
@@ -0,0 +1,26 @@
+typedef unsigned long long ull;
+
+/*
+type operator*(const type& a, const type& b);
+*/
+
+template <typename T> T binexp(T a, ull b) {
+  T res = 1; //@ identity
+  while(b > 0) {
+    if(b & 1) res = res * a;
+    a = a * a;
+    b /= 2;
+  }
+  return res;
+}
+
+template <typename T> T binexp(T a, ull b, T m) {
+  a = a % m;
+  T res = 1;
+  while(b > 0) {
+    if(b & 1) res = res * a % m;
+    a = a * a % m;
+    b /= 2;
+  }
+  return res;
+}
