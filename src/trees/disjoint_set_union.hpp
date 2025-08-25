e5c585951e98693101dd2934ea42ed7572d836f2
Sebastian Certuche <sebascertuche@gmail.com>
2025-08-25 17:16:20 -0600

    Update DSU to be generic

--- src/trees/disjoint_set_union.hpp
+++ src/trees/disjoint_set_union.hpp
@@ -6,26 +6,26 @@ typedef long long lli;
 
 #define fora(i, a, b) for(lli i = (a); (i) < (b); ++i) // [a -> b)
 
-struct DSU {
+template <typename T> struct DSU {
   struct Mt {
-    lli r; //@ state
+    T r;   //@ state
     Mt() { //@ initial state
       r = 1;
     }
   };
-  vector<lli> p; // parents
-  vector<Mt> m;  // metadata
+  vector<T> p;  // parents
+  vector<Mt> m; // metadata
   // O(N)
-  DSU(lli n) : p(n), m(n) { fora(i, 0, n) p[i] = i; }
+  DSU(T n) : p(n), m(n) { fora(i, 0, n) p[i] = i; }
   // O(1)
-  lli find(lli x) { return p[x] == x ? x : (p[x] = find(p[x])); }
-  bool unite(lli x, lli y) { // O(1)
-    lli xrt = find(x), yrt = find(y);
+  T find(T x) { return p[x] == x ? x : (p[x] = find(p[x])); }
+  bool unite(T x, T y) { // O(1)
+    T xrt = find(x), yrt = find(y);
     if(xrt == yrt) return false;
     if(m[xrt].r < m[yrt].r) swap(xrt, yrt);
     m[xrt].r += m[yrt].r; //@ update state
     p[yrt] = xrt;
     return true;
   }
-  Mt &meta(lli x) { return m[find(x)]; } // O(1)
+  Mt &meta(T x) { return m[find(x)]; } // O(1)
 };
